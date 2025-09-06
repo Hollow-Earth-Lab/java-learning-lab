3a03341abfe6193f62abd82720f543496eacd29c
Hollow-Earth-Lab <Hollow-Earth@hotmail.com>
2025-09-06 06:41:28 +0300

    Initial commit

--- src/AdditionOfTwoNumbers.java
+++ src/AdditionOfTwoNumbers.java
@@ -1,16 +1,32 @@
+// Оцени, пожалуйста, готовый код:
+
+import java.util.Scanner;
+
 public class AdditionOfTwoNumbers {
 
     public static void main(String[] args) {
-        System.out.println("Adding two numbers together");
         
-        int firstNumber = 1;
+        int firstNumber;
+        int secondNumber;
+
+        System.out.println("Hi, let's add up two numbers!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        firstNumber = scanner.nextInt();
         System.out.println("First Number: " + firstNumber);
-        
-        int secondNumber = 2;
+
+        System.out.print("Enter the second number: ");
+        secondNumber = scanner.nextInt();
         System.out.println("Second Number: " + secondNumber);
+
+        System.out.println("Adding two numbers together...");
         
         int result = add(firstNumber, secondNumber);
         System.out.println("Result: " + result);
+        
+        scanner.close();
     }
     
     public static int add(int a, int b) {
