7d25ceff8ca111f9163ee4086ab1698700de7f62
Hollow-Earth-Lab <Hollow-Earth@hotmail.com>
2025-09-14 07:03:41 +0300

    Initial commit

---
+++ 100-Java-Tasks-from-Easy-to-Hard/009_LineReversal/LineReversal.bat
@@ -0,0 +1,6 @@
+cls
+javac LineReversal.java
+
+java LineReversal
+
+@ pause
---
+++ 100-Java-Tasks-from-Easy-to-Hard/009_LineReversal/LineReversal.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class LineReversal {
+
+    public static void main(String[] args) {
+
+        String finalLine;
+
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder builder = new StringBuilder();
+
+        System.out.println("Class LineReversal Loading...");
+        // System.out.println();
+        System.out.print("Enter a string of characters: ");
+
+        String originalLine = scanner.nextLine();
+
+        // System.out.println(originalLine);
+
+        int i = originalLine.length();
+
+        // System.out.println(i);
+        // System.out.println();
+
+        i = i - 1;
+
+        while (i >= 0) {
+            char symbol = originalLine.charAt(i);
+            // System.out.println(symbol);
+            builder.append(symbol);
+            i = i - 1;
+        }
+
+        // System.out.println();
+        finalLine = builder.toString();
+        System.out.println("Reverse string of characters: " + finalLine);
+
+        scanner.close();
+
+    }
+}
