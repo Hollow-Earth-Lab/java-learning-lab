f6d5c3a510bab285cd2e09447b93328779ed7053
Hollow-Earth-Lab <Hollow-Earth@hotmail.com>
2025-09-22 22:13:54 +0300

    Initial commit

---
+++ 100-Tasks-from-Easy-to-Hard/016_TemperatureConversion/TemperatureConversion.bat
@@ -0,0 +1,5 @@
+javac TemperatureConversion.java
+
+java TemperatureConversion
+
+pause
---
+++ 100-Tasks-from-Easy-to-Hard/016_TemperatureConversion/TemperatureConversion.java
@@ -0,0 +1,6 @@
+public class TemperatureConversion {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Loading TemperatureConversion class");
+    }
+}
